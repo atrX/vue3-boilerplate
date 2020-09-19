7eea29b69d243e7d207861fd67fe3bca23508c47
Jordy Rymenants <jordy.rymenants@outlook.com>
2020-09-19 16:11:54 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <hello-world msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const test = ref('test');
+
+    return { test };
+  },
+});
+</script>
