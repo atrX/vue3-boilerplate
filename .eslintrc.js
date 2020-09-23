389b3a02c993bace2d3fc6f5a37bfc1306786789
Jordy Rymenants <jordy.rymenants@outlook.com>
2020-09-23 16:36:55 +0200

    chore: update linter config
    chore: update linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,42 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', 120],
+    'lines-between-class-members': 'off',
+    curly: ['error', 'all'],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always-and-inside-groups',
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          { pattern: '@/components/**', group: 'internal' },
+          { pattern: '@/router/**', group: 'internal' },
+          { pattern: '@/store/**', group: 'internal' },
+          { pattern: '@/views/**', group: 'internal' },
+          { pattern: '@/**', group: 'internal', position: 'after' },
+        ],
+      },
+    ],
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
